53f52b6273215b0fab5db63d820c57837017c758
yeseppuku <yakzogfx@gmail.com>
2026-03-23 19:17:40 +0200

    Add files via upload

--- CMakeProject1.cpp
+++ CMakeProject1.cpp
@@ -1,109 +1,79 @@
 ﻿#include <stdio.h>
 
 int main() {
-    int optiune;
-    int valid;
+    int n;
 
-    do {
-        printf("\n====================================");
-        printf("\n          MENIU INTERACTIV          ");
-        printf("\n====================================");
-        printf("\n1. Maximul si frecventa acestuia");
-        printf("\n2. CMMDC si CMMMC");
-        printf("\n3. Conversie in baza b (b < 10)");
-        printf("\n4. Primele n numere Fibonacci");
-        printf("\n5. Iesire");
-        printf("\n====================================");
-        printf("\nAlegeti o optiune (1-5): ");
+    while (1) { // In C, folosim 1 pentru true
+        printf("\nIntroduceti un numar (sau un numar negativ pentru a iesi): ");
 
-        // Citim si verificam daca s-a introdus un numar valid
-        // scanf returneaza numarul de elemente citite cu succes (1 in cazul nostru)
-        valid = scanf("%d", &optiune);
-
-        if (valid != 1) {
-            printf("\n!!! EROARE: Ati introdus un caracter nevalid. Va rugam introduceti o cifra intre 1 si 5.\n");
-
-            // CURATARE BUFFER: Citim caracterele ramase in buffer pana la 'newline' (\n)
-            // Fara asta, programul ar intra intr-o bucla infinita
+        // Citim numarul. Folosim %d pentru intregi.
+        if (scanf("%d", &n) != 1) {
+            // Curatam buffer-ul in caz de input gresit (litere)
             while (getchar() != '\n');
-
-            optiune = 0; // Resetam optiunea pentru a forța reafișarea meniului
-            continue;    // Revenim la inceputul buclei do-while
+            continue;
         }
 
-        switch (optiune) {
-        case 1: {
-            int n, i, numar, max, frecventa = 0;
-            printf("Cati termeni are sirul (n)? ");
-            scanf("%d", &n);
-            if (n <= 0) {
-                printf("Eroare: n trebuie sa fie > 0!\n");
+        // Conditia de exit din program
+        if (n < 0) {
+            printf("Programul s-a incheiat.\n");
+            break;
+        }
+
+        if (n == 0) {
+            printf("zero\n");
+            continue;
+        }
+
+        if (n >= 1000) {
+            printf("Eroare: Te rog introdu un numar mai mic decat 1000.\n");
+            continue;
+        }
+
+        printf("In litere: ");
+
+        // 1. Sute
+        int sute = n / 100;
+        if (sute > 0) {
+            if (sute == 1) {
+                printf("o suta ");
+            }
+            else if (sute == 2) {
+                printf("doua sute ");
             }
             else {
-                printf("Introduceti numerele:\n");
-                for (i = 0; i < n; i++) {
-                    scanf("%d", &numar);
-                    if (i == 0) { max = numar; frecventa = 1; }
-                    else if (numar > max) { max = numar; frecventa = 1; }
-                    else if (numar == max) { frecventa++; }
-                }
-                printf("Maxim: %d, Aparitii: %d\n", max, frecventa);
+                // In C folosim char* pentru siruri de caractere
+                char* cifre[] = { "", "", "", "trei", "patru", "cinci", "sase", "sapte", "opt", "noua" };
+                printf("%s sute ", cifre[sute]);
             }
-            break;
         }
 
-        case 2: {
-            int a, b, m_orig, n_orig, r;
-            printf("Introduceti m si n: ");
-            scanf("%d %d", &m_orig, &n_orig);
-            a = m_orig; b = n_orig;
-            while (b != 0) { r = a % b; a = b; b = r; }
-            int cmmdc = a;
-            long long cmmmc = (long long)m_orig * n_orig / cmmdc;
-            printf("CMMDC = %d, CMMMC = %lld\n", cmmdc, cmmmc);
-            break;
-        }
+        int rest = n % 100;
+        int zeci = rest / 10;
+        int unitati = rest % 10;
 
-        case 3: {
-            int n_num, b, rest;
-            long long rezultat = 0, p = 1;
-            printf("Introduceti n si baza b (<10): ");
-            scanf("%d %d", &n_num, &b);
-            int copie = n_num;
-            if (n_num == 0) printf("Rezultat: 0\n");
-            else {
-                while (copie > 0) {
-                    rest = copie % b;
-                    rezultat += (long long)rest * p;
-                    p *= 10;
-                    copie /= b;
-                }
-                printf("Rezultat: %lld\n", rezultat);
+        // 2. Zeci si Unitati
+        if (zeci == 0) {
+            char* u[] = { "", "unu", "doi", "trei", "patru", "cinci", "sase", "sapte", "opt", "noua" };
+            if (unitati > 0) printf("%s", u[unitati]);
+        }
+        else if (zeci == 1) {
+            char* speciale[] = { "zece", "unsprezece", "doisprezece", "treisprezece", "paisprezece",
+                                 "cincisprezece", "saisprezece", "sapteprezece", "optsprezece", "nouasprezece" };
+            printf("%s", speciale[unitati]);
+        }
+        else {
+            char* z[] = { "", "", "douazeci", "treizeci", "patruzeci", "cincizeci",
+                          "saizeci", "saptezeci", "optzeci", "nouazeci" };
+            printf("%s", z[zeci]);
+
+            if (unitati > 0) {
+                printf(" si ");
+                char* u[] = { "", "unu", "doi", "trei", "patru", "cinci", "sase", "sapte", "opt", "noua" };
+                printf("%s", u[unitati]);
             }
-            break;
         }
-
-        case 4: {
-            int n_fib, f1 = 0, f2 = 1, f3, i;
-            printf("Cati termeni Fibonacci? ");
-            scanf("%d", &n_fib);
-            for (i = 1; i <= n_fib; i++) {
-                if (i == 1) printf("%d ", f1);
-                else if (i == 2) printf("%d ", f2);
-                else { f3 = f1 + f2; printf("%d ", f3); f1 = f2; f2 = f3; }
-            }
-            printf("\n");
-            break;
-        }
-
-        case 5:
-            printf("La revedere!\n");
-            break;
-
-        default:
-            printf("\n!!! OPTIUNE INEXISTENTA: Alegeti doar intre 1 si 5.\n");
-        }
-    } while (optiune != 5);
+        printf("\n"); // Noua linie dupa fiecare numar afisat
+    }
 
     return 0;
 }
